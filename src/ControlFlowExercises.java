ee3fbd59cf227df7b689bdc3813de37f8ba5fcd9
Felecia Davis <felecia.m.davis1@gmail.com>
2021-04-26 17:00:17 -0400

    feat: add ControlFlowExercises.java.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+    public static void main(String[] args){
+//    int i = 5;
+//        System.out.print("i is ");
+//    while (i <= 15) {
+//        //System.out.print(" ");
+//        System.out.print(i + " ");
+//        i++;
+//    }
+//        System.out.println(" ");
+//        int count = 0;
+//        do {
+//            System.out.println(count);
+//            count= count + 2;
+//        } while (count <= 100);
+
+//        System.out.println(" ");
+//        int num = 100;
+//        do {
+//            System.out.println(num);
+//            num = num - 5;
+//        } while (num >= -10);
+
+//        System.out.println(" ");
+//        long num = 2;
+//        do {
+//            System.out.println(num);
+//            num = num * num;
+//        } while (num < 1000000);
+
+//            System.out.print(" i is ");
+//        for(int i = 5; i <= 15; i++) {
+//            System.out.print(i + " ");
+//        }
+
+//        System.out.print(" ");
+//        for(int i = 100; i >= -10; i= i - 5) {
+//            System.out.println(i + " ");
+//        }
+
+//        System.out.print(" ");
+//        for(long i = 2; i < 1000000; i= i * i) {
+//            System.out.println(i + " ");
+//        }
+
+//        Write a program that prints the numbers from 1 to 100.
+//        For multiples of three: print “Fizz” instead of the number.
+//        For the multiples of five: print “Buzz”.
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
+
+//        for(int i = 1; i <= 100; i++) {
+//            if((i % 3 == 0)  && (i % 5 == 0)) {
+//                System.out.println("FizzBuzz");
+//            }else if(i % 3 == 0){
+//            System.out.println("Fizz");
+//        }else if( i % 5 == 0){
+//                System.out.println("Buzz");
+//            }else { System.out.println(i);
+//                }
+//            }
+
+        Scanner newTable = new Scanner(System.in);
+        System.out.println("");
+
+
+
+    }
+}
---
+++ src/ControlStmtsLoopsLec.java
@@ -0,0 +1,9 @@
+public class ControlStmtsLoopsLec {
+    public static void main(String[] args){
+
+        System.out.println("(5 == 5))="  + (5 ==5));
+        boolean isAdmin = true;
+        System.out.println("(isAdmin == false)  = " + (isAdmin == true));
+
+    }
+}
