aca707e07ab073b2d89e15d3480e4e3be635c7cc
Felecia Davis <felecia.m.davis1@gmail.com>
2021-04-29 15:40:19 -0400

    feat: add oopsLecture.

--- src/ControlStmtsLoopsLec.java
+++ src/ControlStmtsLoopsLec.java
@@ -1,9 +1,15 @@
 public class ControlStmtsLoopsLec {
     public static void main(String[] args){
 
-        System.out.println("(5 == 5))="  + (5 ==5));
-        boolean isAdmin = true;
-        System.out.println("(isAdmin == false)  = " + (isAdmin == true));
+//        System.out.println("(5 == 5))="  + (5 ==5));
+//        boolean isAdmin = true;
+//        System.out.println("(isAdmin == false)  = " + (isAdmin == true));
+int x = 5;
+System.out.println(x >2 ? x < 4 ? 10 : 8: 7);
 
+//        int x = 5 * 4 % 3;
+//        String num = "this";
+//
+//        System.out.println(num + x);
     }
 }
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,7 +8,7 @@ public class MethodsExercises {
         System.out.println(division(6, 2));
         System.out.println(modulus(1, 2));
         System.out.println(multiply(2, 3));
-        System.out.println(getInteger(0, 10));
+        //System.out.println(getInteger(1, 10));
 
     }
 
@@ -59,29 +59,48 @@ public class MethodsExercises {
 //        return result; }
 
 
-    public static int getInteger(int min, int max){
-        Scanner practice = new Scanner(System.in);
-
-    //while((min < 1 ) ||  (max > 10)) {
-        if((min < 1 ) ||  (max > 10)) {
-            System.out.print("Enter two numbers between 1 and 10: ");
-            int less = practice.nextInt();
-            int greater = practice.nextInt();
-        }else (System.out.print("Numbers are in range.");
-        //getInteger(less, greater);
-
-
-
-    }
-
-
-
-        //int intNum = practice.nextInt();
-//        f (userInput > min && userInput < max) {
-//            System.out.printf("Congrats, your input of %d was within our min and max!", userInput);
-//            return userInput;
+//    public static int getInteger(int min, int max) {
+//        Scanner practice = new Scanner(System.in);
+//        System.out.println("Enter a number between " + min + " and " + max);
+//        int num = practice.nextInt();
+//
+//        if (num > min && num < max) {
+//            System.out.println("Yes, your number is between " + min + " and " + max);
+//            return num;
+//
+//        } else {
+//            System.out.println("Your number is not in range.");
+//            return getInteger(min, max);
 //        }
+//    }
+//Calculate the factorial of a number.
+//
+//    Prompt the user to enter an integer from 1 to 10.
+//    Display the factorial of the number entered by the user.
+//    Ask if the user wants to continue.
+//    Use a for loop to calculate the factorial.
+//    Assume that the user will enter an integer, but verify it’s between 1 and 10.
+//    Use the long type to store the factorial.
+//    Continue only if the user agrees to.
+//    A factorial is a number multiplied by each of the numbers before it.
+//    Factorials are denoted by the exclamation point (n!).
+//    Ex:
+//            1! = 1               = 1
+//            2! = 1 x 2           = 2
+//            3! = 1 x 2 x 3       = 6
+//            4! = 1 x 2 x 3 x 4   = 24
 
+//    public static void factorial() {
+//        Scanner numFactorial = new Scanner(System.in);
+//
+//       System.out.println("Enter an integer between 1 and 10.");
+//       long num = numFactorial.nextLong();
+//       for(int i = 1; i <= num; i++){
+//           for(int j = 1; j <= num; j++){
+//               System.out.println(i + "!")
+         // }
+       //}
+//}
     }
-   // }
+
 
