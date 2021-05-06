50c193ca178cc97f2598ad09840d6c17f508129f
Felecia Davis <felecia.m.davis1@gmail.com>
2021-05-06 11:05:38 -0400

    add movies exercise for array module.

---
+++ src/movies/Movie.java
@@ -0,0 +1,19 @@
+package movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
